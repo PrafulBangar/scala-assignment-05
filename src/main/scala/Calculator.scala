23d8dee6f92543a99ba91c684d7d398bf3107b09
PrafulBangar <prafulbangar581@gmail.com>
2019-07-16 12:01:44 +0530

    meged to master

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -19,13 +19,4 @@ class Calculator {
     number1/number2
   }
 }
-/*
-object Calcuculator extends  App{
 
-  val obj =new Calculator
-  println(obj.addOperation(3,5))
-  println(obj.subOperation(7,2))
-  println(obj.multiplicationOperation(3,7))
-  println(obj.devideOperation(10,0))
-}
-*/
